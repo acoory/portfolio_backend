27e2ff28f4246bb4eb9bca80915b8ac598a7f79a
Cory Anthony <anthony.cory@epitech.eu>
2025-10-23 06:05:26 +0200

    feat(auth): add registration restriction plugin and improve auth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,44 @@
-import { betterAuth } from 'better-auth';
-import { Pool } from 'pg';
+import { betterAuth, BetterAuthPlugin } from 'better-auth';
 import { PrismaClient } from '../../generated/prisma';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { createAuthMiddleware } from 'better-auth/plugins';
 
 const prisma = new PrismaClient();
 
+const myPlugin = () => {
+  return {
+    id: 'disable-registrations-plugin',
+    hooks: {
+      before: [
+        {
+          matcher: (context: any) => {
+            if (context.path === '/sign-up/email') {
+              return true;
+            }
+            return false;
+          },
+          handler: createAuthMiddleware(async (ctx) => {
+            if (ctx.path === '/sign-up/email') {
+              return {
+                status: 403,
+                body: { message: 'Registrations are disabled.' },
+              };
+            }
+          }),
+        },
+      ],
+    },
+  } satisfies BetterAuthPlugin;
+};
+
 export const auth = betterAuth({
+  plugins: [myPlugin()],
   baseURL: process.env.BETTER_AUTH_URL || 'http://localhost:3000',
-  trustedOrigins: ['http://localhost:5173', 'http://localhost:3000'],
+  trustedOrigins: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://honydev.fr',
+  ],
   emailAndPassword: {
     enabled: true,
   },
